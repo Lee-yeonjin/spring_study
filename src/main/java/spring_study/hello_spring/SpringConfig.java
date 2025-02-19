f6c8214046c0aed7735a340b2cd0e568a2bff119
yeonjin <lyjduswls37@naver.com>
2025-02-19 19:17:38 +0900

    순수 JDBC

--- src/main/java/spring_study/hello_spring/SpringConfig.java
+++ src/main/java/spring_study/hello_spring/SpringConfig.java
@@ -1,13 +1,23 @@
 package spring_study.hello_spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring_study.hello_spring.repository.JdbcMemberRepository;
 import spring_study.hello_spring.repository.MemberRepository;
-import spring_study.hello_spring.repository.MemoryMemberRepository;
 import spring_study.hello_spring.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
